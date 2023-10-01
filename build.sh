edb7f95c82781630382a7866aa90fee40d9d161c
Bryan Gonzalez <bgonza868@gmail.com>
2023-10-01 23:37:53 +0000

    reorder

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 ANDROID_HOME=${ANDROID_HOME:-${HOME}/android-sdk}
-
-bash ./install-build-deps.sh
-
 DOTNET_ROOT=${HOME}/.dotnet
 PATH=${DOTNET_ROOT}:${PATH}
+
+bash ./install-build-deps.sh
 
 dotnet --info
 dotnet build -p:AndroidSdkDirectory=${ANDROID_HOME} SheepBrowser/
